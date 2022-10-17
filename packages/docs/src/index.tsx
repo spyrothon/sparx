1a9ef88c15857408c8005fab0a4ac95d1b493a7d
faultyserver <jonegeland@gmail.com>
2022-10-17 01:49:50 -0400

    add "default" styles to package

--- packages/docs/src/index.tsx
+++ packages/docs/src/index.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-import "../.design_system/generated/DesignSystem.css";
 import "@spyrothon/sparx/dist/style.css";
+import "@spyrothon/sparx/dist/default.css";
 
 const root = createRoot(document.getElementById("app-container")!);
 root.render(<App />);
