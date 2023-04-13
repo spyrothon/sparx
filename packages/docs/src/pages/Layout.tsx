06fd54b41b205dbeca804a8bbdfc4dfbe37f9915
faultyserver <jonegeland@gmail.com>
2023-04-12 21:13:35 -0700

    Add basic tabs example

--- packages/docs/src/pages/Layout.tsx
+++ packages/docs/src/pages/Layout.tsx
@@ -1,7 +1,34 @@
 import * as React from "react";
-import { Stack, Text } from "@spyrothon/sparx";
+import { Card, Section, Stack, Tabs, Tag, Text } from "@spyrothon/sparx";
 
 import PageHeader from "./PageHeader";
+
+function TabsComponent() {
+  return (
+    <Stack as={Section} spacing="space-lg">
+      <Card>
+        <Tabs.Group>
+          <Tabs.Header label="Group 1" />
+          <Tabs.Tab label="Item 1" />
+          <Tabs.Tab label="Item 2" />
+          <Tabs.Tab label="Item 3" color="accent" />
+          <Tabs.Header label="Group 2" />
+          <Tabs.Tab label="Item 4" color="success" badge="15" />
+          <Tabs.Tab label="Item 5" color="danger" badge={12} />
+          <Tabs.Tab
+            label="Item 6"
+            badge={
+              <Tag color="success" solid>
+                New
+              </Tag>
+            }
+          />
+          <Tabs.Tab label="Item 7" />
+        </Tabs.Group>
+      </Card>
+    </Stack>
+  );
+}
 
 export default function Layout() {
   return (
@@ -11,6 +38,7 @@ export default function Layout() {
         Layout components provide utilities for managing how multiple components get positioned
         together and affect the flow of content.
       </Text>
+      <TabsComponent />
     </Stack>
   );
 }
