1f639e9cd6902ddd7cbd7a1dd2249ed25ba0c5ec
faultyserver <jonegeland@gmail.com>
2023-07-31 10:35:43 -0700

    ForwardRef almost all inputs and maintain compatibility for form libraries

--- packages/sparx/src/components/forms/formatted_inputs/DateTimeInput.tsx
+++ packages/sparx/src/components/forms/formatted_inputs/DateTimeInput.tsx
@@ -5,23 +5,26 @@ import { formatDateTimeLocalToString } from "@sparx/utils/DateTimeUtils";
 
 export interface DateTimeInputProps extends Omit<TextInputProps, "type" | "value" | "onChange"> {
   value?: Date;
-  onChange: (value: Date) => unknown;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>, value: Date) => unknown;
 }
 
-export function DateTimeInput(props: DateTimeInputProps) {
-  const { value, onChange, ...otherProps } = props;
+export const DateTimeInput = React.forwardRef<HTMLInputElement, DateTimeInputProps>(
+  function DateTimeInput(props, ref) {
+    const { value, onChange, ...otherProps } = props;
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const value = new Date(event.target.value);
-    onChange(value);
-  }
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+      const value = new Date(event.target.value);
+      onChange(event, value);
+    }
 
-  return (
-    <TextInput
-      {...otherProps}
-      type="datetime-local"
-      value={formatDateTimeLocalToString(value)}
-      onChange={handleChange}
-    />
-  );
-}
+    return (
+      <TextInput
+        ref={ref}
+        {...otherProps}
+        type="datetime-local"
+        value={formatDateTimeLocalToString(value)}
+        onChange={handleChange}
+      />
+    );
+  },
+);
