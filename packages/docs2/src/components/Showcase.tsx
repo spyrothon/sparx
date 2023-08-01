07e7cf744a3ffd11b39b71567c9c91180c939108
faultyserver <jonegeland@gmail.com>
2023-08-01 15:29:34 -0700

    syntax highlighting

--- packages/docs2/src/components/Showcase.tsx
+++ packages/docs2/src/components/Showcase.tsx
@@ -1,22 +1,40 @@
 "use client";
 
-import { Card, Stack, Text } from "@spyrothon/sparx";
+import * as React from "react";
+import { Stack } from "@spyrothon/sparx";
+import { toH } from "hast-to-hyperscript";
+import { refractor } from "refractor";
+import tsx from "refractor/lang/tsx";
+import bash from "refractor/lang/bash";
+import css from "refractor/lang/css";
 
 import styles from "./Showcase.module.css";
+
+import "./syntaxHighlighting.css";
+
+refractor.register(bash);
+refractor.register(css);
+refractor.register(tsx);
 
 interface ShowcaseProps {
   content: React.ReactNode;
   source: string;
+  language: "tsx" | "jsx" | "bash";
 }
 
 export function Showcase(props: ShowcaseProps) {
-  const { content, source } = props;
+  const { content, source, language = "tsx" } = props;
+  const highlighted = React.useMemo(
+    () => toH(React.createElement, refractor.highlight(source, language)),
+    [source, language],
+  );
+
   return (
     <Stack className={styles.showcase} spacing="space-none">
       <div className={styles.content}>{content}</div>
-      <Text variant="text-sm/normal" className={styles.source}>
-        <pre>{source}</pre>
-      </Text>
+      <pre className={styles.source + ` refractor-highlight language-${language}`}>
+        <code>{highlighted}</code>
+      </pre>
     </Stack>
   );
 }
